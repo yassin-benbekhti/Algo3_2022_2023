82f582b97519470a8bee02c30158ce6bb40d3a03
yassin-benbekhti <113033834+yassin-benbekhti@users.noreply.github.com>
2022-10-12 11:06:19 +0200

    Add files via upload

---
+++ main ex3.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+ int suite (int n)
+{
+    if(n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return 2+suite(n-1);
+    }
+}
+
+int puissance_recurs( int  a, int n)
+{
+    if (a==0  and n==0)
+    {
+        return 1;
+    }
+    else{
+        if (a=0)
+        {
+            return 0;
+        }
+        else{
+            if ( n==1)
+                {
+                    return a;
+                }
+                else
+                {
+                    return a*recursive_puissance(a,n-1);
+                }
+            }
+
+
+         }
+
+}
+
+int main()
+{
+    printf("%d\n",suite(5));
+    printf("%d\n",puissance _recurs(5,8));
+}
