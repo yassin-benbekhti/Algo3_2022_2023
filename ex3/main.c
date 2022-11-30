aa01548873c1a44e3f69c0e65c0dd73891eb5388
yassin-benbekhti <113033834+yassin-benbekhti@users.noreply.github.com>
2022-11-30 16:09:00 +0100

    Update main.c

--- ex3/main.c
+++ ex3/main.c
@@ -3,9 +3,9 @@
      Client c;
      FILE *f=fopen(nomFichier,"r");
      while(!feof(f)){
-         fin=fscanf(f," id %d nom %s tel %s ", &c.id,c.nom,c.tel);
+         fin=fscanf(f," id %d nom %s ", & c.id,c.nom);
          if(fin != EOF){
-             printf(" id %d nom %s tel %s ", c.id, c.nom, c.tel);
+             printf(" id %d nom %s ", c.id, c.nom);
              }
      }
      fclose(f);
